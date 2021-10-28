2a0d525f46f5e7f33300cfbc89c9f1d33e14debc
Rashid Ahmad <raashid.ahmad@gmail.com>
2021-10-28 11:41:21 +0500

    changes from client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'New Project';
+  title = 'New Project. This is change from the client';
 }
